0817f34941480adfebe0d2929a29463e9b8f8efb
chenyue404 <601450225@qq.com>
2021-11-04 09:58:47 +0800

    初始导入

---
+++ app/src/main/java/com/chenyue404/BrowserOperator/App.kt
@@ -0,0 +1,23 @@
+package com.chenyue404.BrowserOperator
+
+import android.app.Application
+import android.content.Context
+
+/**
+ * Created by cy on 2021/11/4.
+ */
+class App : Application() {
+
+    companion object {
+        lateinit var gContext: App
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
+        gContext = this
+    }
+}
